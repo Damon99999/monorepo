7cdbebd740b03fc240c3e2efb15c037c4d9a0657
liuchaoming <chaoming.liu@akuvox.com>
2023-05-15 11:55:49 +0800

    feat: monorepo第一次更新

---
+++ packages/demo1/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { windowAlert } from 'akubela-utils'
+import { Button, Switch } from 'akubela-components'
+windowAlert('我是demo1')
+</script>
+<template>
+  <div>
+    <h1>我是demo1</h1>
+    <Button></Button>
+    <Switch />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ packages/demo2/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { windowAlert } from 'akubela-utils'
+import { Button, Switch } from 'akubela-components'
+windowAlert('我是demo2')
+</script>
+
+<template>
+  <div>
+    <h1>我是demo2</h1>
+    <Button></Button>
+    <Switch />
+  </div>
+</template>
+
+<style scoped>
+</style>
